21197c2e24e8f96a97e7b601ca0695f4aa35716e
puuury <puuubuntu@gmail.com>
2024-11-26 14:01:31 +0330

    json2

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 
-    let animation = lottie.loadAnimation({
-        container: document.getElementById('lottie-animation'), 
-        renderer: 'svg',
-        loop: true, 
-        autoplay: true,
-        path: '/lottie/a.json',
+    // let animation = lottie.loadAnimation({
+    //     container: document.getElementById('lottie-animation'), 
+    //     renderer: 'svg',
+    //     loop: true, 
+    //     autoplay: true,
+    //     path: '/lottie/a.json',
         
+    // });
+    // animation.setSpeed(3);
+    var animation = lottie.loadAnimation({
+        container: document.getElementById('lottie-animation'), // عنصر DOM که انیمیشن در آن نمایش داده می‌شود
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'lottie/a.json' // مسیر فایل JSON
     });
-    animation.setSpeed(3);
-
+    
