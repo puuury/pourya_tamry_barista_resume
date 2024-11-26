75d60d26fccbe72694f7b1b64bd1ee85686488d1
puuury <puuubuntu@gmail.com>
2024-11-26 13:33:04 +0330

    middle

---
+++ app.js
@@ -0,0 +1,11 @@
+
+    let animation = lottie.loadAnimation({
+        container: document.getElementById('lottie-animation'), 
+        renderer: 'svg',
+        loop: true, 
+        autoplay: true,
+        path: '/lottie/Animation - 1732608899613.json',
+        
+    });
+    animation.setSpeed(3);
+
